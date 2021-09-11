f654653d1da1bcdafb97ad6072179cbb2a280fc4
Umer Abdullah Yaseen <umerabdillah6223@gmail.com>
2021-09-12 03:16:40 +0500

    setting axios defualt global configurations

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
-axios.interceptors.request.use(request=>{
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'Auth Token';
+axios.defaults.headers.post['Content-Type'] = 'application/json'; 
+
+
+
+axios.interceptors.request.use(request=>{   
     console.log(request);
     return request;
 }, error=>{
